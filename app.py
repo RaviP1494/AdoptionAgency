da1477ca05281a28136d12445a324df225ddb3ac
FlowOfASun <person1494@gmail.com>
2022-09-26 09:34:00 -0500

    end

--- app.py
+++ app.py
@@ -1,25 +1,59 @@
 from flask import Flask, request, render_template, redirect, flash
-from forms import AddTemplateForm
+from forms import AddPetForm, EditPetForm
+from models import db, connect_db, Pet
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "thisisthekey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:drowssap@localhost:5432/adoption'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('pets.html', pets = pets)
 
 @app.route("/add", methods=["GET", "POST"])
-def add_snack():
-    """Snack add form; handle adding."""
+def add():
+    """Pet add form; handle adding."""
 
-    form = AddTemplateForm()
-
+    form = AddPetForm()
+    
     if form.validate_on_submit():
         name = form.name.data
-        weight = form.weight.data
-        flash(f"Added {name} at {weight}")
-        return redirect("/add")
+        species = form.species.data
+        img_url = form.img_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        p = Pet(name = name, species = species, img_url = img_url, age = age, notes = notes)
+        db.session.add(p)
+        db.session.commit()
+
+        return redirect("/")
 
     else:
-        return render_template("/add.html", form=form)
+        return render_template("/add.html", form = form)
+
+@app.route("/pets/<int:pid>", methods=["GET", "POST"])
+def pet_view(pid):
+    """Show individual pet details, along with edit form and edit form handling"""
+    
+    pet = Pet.query.get(pid)
+
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.img_url = form.img_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        return redirect("/")
+    else:
+        print('what is going on')
+        return render_template('/pet.html', p = pet, form = form)
--- forms.py
+++ forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Optional, AnyOf, URL, NumberRange
 
-class AddTemplateForm(FlaskForm):
-    """Template Form"""
+class AddPetForm(FlaskForm):
+    """Pet Add Form"""
 
-    name = StringField("Name")
-    weight = FloatField("Weight")
+    name = StringField("Name", validators=[InputRequired()])
+    species = StringField("Species", validators=[InputRequired(), AnyOf("dog", "cat", "porcupine")])
+    img_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = IntegerField("Age", validators=[NumberRange(min=0, max=30)])
+    notes = StringField("Notes", validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """Pet Edit Form"""
+
+    img_url = StringField("Photo URL", validators=[Optional(), URL()])
+    notes = StringField("Notes", validators=[Optional()])
+    available = BooleanField("Available")
